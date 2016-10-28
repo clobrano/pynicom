3706809a6fce5cb958c7de2f9f91e5f269d18c3f
Carlo Lobrano <carlo.lobrano@telit.com>
2016-10-28 15:25:09 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from setuptools import setup, find_packages
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
