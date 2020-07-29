f5bf5c4e613e18640b0f1b05ac24c7e82ad996e3
Carlo Lobrano <c.lobrano@gmail.com>
2020-07-29 14:28:05 +0200

    update version to 3.5.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 from setuptools import setup, find_packages
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
